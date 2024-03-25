b9d3f371b27bb4025bae45dcab708c2f3327a97e
Viktor <128511467+Someonemann@users.noreply.github.com>
2024-03-25 08:07:00 -0400

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -1,13 +1,13 @@
 def draw_hangman(attempts):
-    # Function to draw the hangman based on the number of attempts left
+    # Function to draw the hangman based on the number  left
     pass  # Placeholder, actual implementation to be added
-
+# function to display words, and things with guesse_letters variable
 def display_word(word, guessed_letters):
     displayed_word = ""
     for letter in word:
         if letter in guessed_letters:
-            displayed_word += letter + " "
+            displayed_word += letter + " " #will add letter, if it the guess is right
         else:
             displayed_word += "_ "
-    return displayed_word.strip()  # Strip removes any leading/trailing whitespace
+    return displayed_word.strip()  # Strip will clean any leading/trailing whitespace
 
