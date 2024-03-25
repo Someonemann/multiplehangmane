665bb9fe6742eb311fb99155221617392b570738
Viktor <128511467+Someonemann@users.noreply.github.com>
2024-03-25 08:18:55 -0400

    Update words.py

--- words.py
+++ words.py
@@ -1,5 +1,5 @@
+# we have list with the some words, and module random, that will generate random woords from our list
 import random
-
 def choose_word():
     words = ["python", "hangman", "programming", "computer", "game", "developer", "coding"]
     return random.choice(words)
